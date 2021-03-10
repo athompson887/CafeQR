261acfea9b8100206fa8315be34cc950e853eac4
Andrew Thompson <andrew.thompson@firstchoice-cs.co.uk>
2021-03-10 13:29:45 +0000

    works with full cycle of login,register and logout

--- merchant/src/main/java/com/athompson/cafe/ui/activities/LoginActivity.kt
+++ merchant/src/main/java/com/athompson/cafe/ui/activities/LoginActivity.kt
@@ -2,7 +2,6 @@ package com.athompson.cafe.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import android.view.LayoutInflater
 import android.view.WindowManager
 import androidx.lifecycle.lifecycleScope
